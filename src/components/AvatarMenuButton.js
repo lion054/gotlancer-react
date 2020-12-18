f84b311b9e13399aa51015ada1a604ca37b176e9
lion054 <lionel.siduna@gmail.com>
2020-12-19 02:23:08 +0800

    Make avatar button as hoverable

--- src/components/AvatarMenuButton.js
+++ src/components/AvatarMenuButton.js
@@ -1,15 +1,12 @@
 import React, { Fragment, PureComponent } from 'react';
 import {
-  ButtonBase,
-  ClickAwayListener,
-  Grow,
+  Button,
   ListItemIcon,
   ListItemText,
+  Menu,
   MenuItem,
-  MenuList,
-  Paper,
-  Popper,
   Switch,
+  Typography,
   withStyles
 } from '@material-ui/core';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
@@ -18,21 +15,11 @@ import { faCog, faCrown, faShoppingCart, faSignOutAlt, faUser, faUserCircle } fr
 const styles = (theme) => ({
   root: {
     borderRadius: 20,
-    paddingLeft: 12
-  },
-  title: {
-    color: '#314963',
-    fontSize: 12,
-    textAlign: 'right'
-  },
-  subtitle: {
-    color: '#ADBDCD',
-    fontSize: 12,
-    textAlign: 'right'
+    marginLeft: 12
   },
   avatarWrapper: {
-    display: 'inline-block',
-    marginLeft: theme.spacing(1.5)
+    marginLeft: theme.spacing(1.5),
+    display: 'inline-block'
   },
   avatarIcon: {
     color: theme.palette.text.disabled,
@@ -49,13 +36,27 @@ const styles = (theme) => ({
   },
   menuIcon: {
     color: '#314963',
-    fontSize: 24
+    fontSize: 20
   },
   menuText: {
     color: '#314963',
     fontSize: 14
   }
 });
+
+const TitleTypography = withStyles({
+  root: {
+    color: '#314963',
+    textAlign: 'right'
+  }
+})(Typography);
+
+const BalanceTypography = withStyles({
+  root: {
+    color: '#ADBDCD',
+    textAlign: 'right'
+  }
+})(Typography);
 
 class AvatarMenuButton extends PureComponent {
   state = {
@@ -71,76 +72,63 @@ class AvatarMenuButton extends PureComponent {
 
   render = () => (
     <Fragment>
-      <ButtonBase
+      <Button
         className={this.props.classes.root}
         aria-describedby={!!this.state.avatarEl ? 'avatar-popover' : undefined}
         onClick={this.onOpenMenu}
       >
         <div style={{ display: 'inline-block' }}>
-          <div className={this.props.classes.title}>Hi, Apurba</div>
-          <div className={this.props.classes.subtitle}>$100.00 USD</div>
+          <TitleTypography variant="body2" display="block">Hi, Apurba</TitleTypography>
+          <BalanceTypography variant="caption" display="block">$100.00 USD</BalanceTypography>
         </div>
         <div className={this.props.classes.avatarWrapper}>
           <FontAwesomeIcon icon={faUserCircle} className={this.props.classes.avatarIcon} />
         </div>
-      </ButtonBase>
-      <Popper
-        open={!!this.state.avatarEl}
+      </Button>
+      <Menu
+        id="avatar-menu"
         anchorEl={this.state.avatarEl}
-        role={undefined}
-        transition
-        disablePortal
+        keepMounted
+        open={!!this.state.avatarEl}
+        onClose={this.onCloseMenu}
       >
-        {({ TransitionProps, placement }) => (
-          <Grow
-            {...TransitionProps}
-            style={{ transformOrigin: placement === 'bottom' ? 'center top' : 'center bottom' }}
-          >
-            <Paper>
-              <ClickAwayListener onClickAway={this.onCloseMenu}>
-                <MenuList autoFocusItem={!!this.state.avatarEl} id="avatar-menu-list">
-                  <MenuItem onClick={this.onCloseMenu} className={this.props.classes.menuItem}>
-                    <ListItemIcon className={this.props.classes.menuIconWrapper}>
-                      <FontAwesomeIcon icon={faCog} className={this.props.classes.menuIcon} />
-                    </ListItemIcon>
-                    <ListItemText className={this.props.classes.menuText} primary="Settings" />
-                  </MenuItem>
-                  <MenuItem onClick={this.onCloseMenu} className={this.props.classes.menuItem}>
-                    <ListItemIcon className={this.props.classes.menuIconWrapper}>
-                      <FontAwesomeIcon icon={faUser} className={this.props.classes.menuIcon} />
-                    </ListItemIcon>
-                    <ListItemText className={this.props.classes.menuText} primary="My Profile" />
-                  </MenuItem>
-                  <MenuItem onClick={this.onCloseMenu} className={this.props.classes.menuItem}>
-                    <ListItemIcon className={this.props.classes.menuIconWrapper}>
-                      <FontAwesomeIcon icon={faCrown} className={this.props.classes.menuIcon} />
-                    </ListItemIcon>
-                    <ListItemText className={this.props.classes.menuText} primary="Membership" />
-                  </MenuItem>
-                  <MenuItem onClick={this.onCloseMenu} className={this.props.classes.menuItem}>
-                    <ListItemIcon className={this.props.classes.menuIconWrapper}>
-                      <FontAwesomeIcon icon={faShoppingCart} className={this.props.classes.menuIcon} />
-                    </ListItemIcon>
-                    <ListItemText className={this.props.classes.menuText} primary="Buy Bid Credit" />
-                  </MenuItem>
-                  <MenuItem onClick={this.onCloseMenu} className={this.props.classes.menuItem}>
-                    <ListItemIcon className={this.props.classes.menuIconWrapper}>
-                      <FontAwesomeIcon icon={faSignOutAlt} className={this.props.classes.menuIcon} />
-                    </ListItemIcon>
-                    <ListItemText className={this.props.classes.menuText} primary="Logout" />
-                  </MenuItem>
-                  <MenuItem onClick={this.onToggleOnline} className={this.props.classes.menuItem}>
-                    <ListItemIcon className={this.props.classes.menuIconWrapper}>
-                      <Switch checked={this.state.online} />
-                    </ListItemIcon>
-                    <ListItemText className={this.props.classes.menuText} primary="Online" />
-                  </MenuItem>
-                </MenuList>
-              </ClickAwayListener>
-            </Paper>
-          </Grow>
-        )}
-      </Popper>
+        <MenuItem onClick={this.onCloseMenu} className={this.props.classes.menuItem}>
+          <ListItemIcon className={this.props.classes.menuIconWrapper}>
+            <FontAwesomeIcon icon={faCog} className={this.props.classes.menuIcon} />
+          </ListItemIcon>
+          <ListItemText classes={{ primary: this.props.classes.menuText }} primary="Settings" />
+        </MenuItem>
+        <MenuItem onClick={this.onCloseMenu} className={this.props.classes.menuItem}>
+          <ListItemIcon className={this.props.classes.menuIconWrapper}>
+            <FontAwesomeIcon icon={faUser} className={this.props.classes.menuIcon} />
+          </ListItemIcon>
+          <ListItemText classes={{ primary: this.props.classes.menuText }} primary="My Profile" />
+        </MenuItem>
+        <MenuItem onClick={this.onCloseMenu} className={this.props.classes.menuItem}>
+          <ListItemIcon className={this.props.classes.menuIconWrapper}>
+            <FontAwesomeIcon icon={faCrown} className={this.props.classes.menuIcon} />
+          </ListItemIcon>
+          <ListItemText classes={{ primary: this.props.classes.menuText }} primary="Membership" />
+        </MenuItem>
+        <MenuItem onClick={this.onCloseMenu} className={this.props.classes.menuItem}>
+          <ListItemIcon className={this.props.classes.menuIconWrapper}>
+            <FontAwesomeIcon icon={faShoppingCart} className={this.props.classes.menuIcon} />
+          </ListItemIcon>
+          <ListItemText classes={{ primary: this.props.classes.menuText }} primary="Buy Bid Credit" />
+        </MenuItem>
+        <MenuItem onClick={this.onCloseMenu} className={this.props.classes.menuItem}>
+          <ListItemIcon className={this.props.classes.menuIconWrapper}>
+            <FontAwesomeIcon icon={faSignOutAlt} className={this.props.classes.menuIcon} />
+          </ListItemIcon>
+          <ListItemText classes={{ primary: this.props.classes.menuText }} primary="Logout" />
+        </MenuItem>
+        <MenuItem onClick={this.onToggleOnline} className={this.props.classes.menuItem}>
+          <ListItemIcon className={this.props.classes.menuIconWrapper}>
+            <Switch checked={this.state.online} />
+          </ListItemIcon>
+          <ListItemText classes={{ primary: this.props.classes.menuText }} primary="Online" />
+        </MenuItem>
+      </Menu>
     </Fragment>
   )
 }
